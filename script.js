58a1920edf55123386869f03200e0b0b5f67782b
Lena Castillo <lenac@uninorte.edu.co>
2025-03-09 00:21:36 -0500

    skills

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var typingEffect = new Typed(".typedText", {
+        strings: ["Developer", "Coder", "Programmer"], // Palabras animadas
+        typeSpeed: 100,  // Velocidad de escritura
+        backSpeed: 80,   // Velocidad de borrado
+        backDelay: 2000, // Pausa antes de borrar
+        loop: true       // Repetir animación
+    });
+});
+
