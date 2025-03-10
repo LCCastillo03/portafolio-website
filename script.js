b3806bcad1c3159ba815adcdd1fd6cc4ac26595b
Lena Castillo <lenac@uninorte.edu.co>
2025-03-09 20:35:18 -0500

    menu

--- script.js
+++ script.js
@@ -11,11 +11,18 @@ document.addEventListener("DOMContentLoaded", function () {
 
 const menuIcon = document.querySelector('#menu-icon');
 const navLinks = document.querySelector('.nav-links');
+const navItems = document.querySelectorAll('.nav-links a'); // Selecciona los enlaces dentro del menú
 
 if (menuIcon && navLinks) {
     menuIcon.onclick = () => {
         navLinks.classList.toggle('active');
     };
+
+    navItems.forEach(link => {
+        link.addEventListener('click', () => {
+            navLinks.classList.remove('active');
+        });
+    });
 } else {
     console.error("No se encontró #menu-icon o .nav-links en el DOM");
 }
